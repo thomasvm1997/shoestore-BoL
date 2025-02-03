492a12c2d5d846a2799cbd5ffee1a305909d10b1
Thomas Van Maele <thomas.van.maele3@student.howest.be>
2025-02-03 18:52:55 +0100

    Add search component and basic styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import ShoeStore from './components/ShoeStore'
+import SearchBarComponent from './components/SearchBarComponent'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <ShoeStore></ShoeStore>
+      <div className="container">
+        <div className="row-1">
+          <h2>BoL Shoes</h2>
+          <SearchBarComponent onSearch={setSearchTerm}></SearchBarComponent>
+        </div>
+        <div className="row-2">
+            <div className="column">Column 1</div>
+            <div className="column">Column 2 (Twice as wide)</div>
+            <div className="column">Column 3</div>
+        </div>
+    </div>
     </>
   )
 }
